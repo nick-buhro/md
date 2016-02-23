562be0dcf7be3fc2438334a40105026cf4425966
Nicholas Buhro <n.buhro@gmail.com>
2016-02-23 11:39:31 +0300

    Solution is created (with 3 projects)
    Created 3 projects: NickBuhro.Markdown, NickBuhro.Markdown.Tests,
    NickBuhro.Markdown.Tests.SpecificationParser.

---
+++ NickBuhro.Markdown.Tests.SpecificationParser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NickBuhro.Markdown.Tests.SpecificationParser
+{
+    internal class Program
+    {
+        private const string SpecSource = @"https://raw.githubusercontent.com/jgm/CommonMark/master/spec.txt";
+
+        public static void Main()
+        {
+            
+        }
+    }
+}
